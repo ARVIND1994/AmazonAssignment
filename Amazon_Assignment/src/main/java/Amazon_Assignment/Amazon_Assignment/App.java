bab6542415cd1e7ca491804731b2670f06f1cbf6
arvind <arvindkumar9057@gmail.com>
2022-05-01 09:26:49 +0530

    First Commit

---
+++ Amazon_Assignment/src/main/java/Amazon_Assignment/Amazon_Assignment/App.java
@@ -0,0 +1,13 @@
+package Amazon_Assignment.Amazon_Assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
